2fc0aa74a07dcdea619c98e9062d9fb98f30af80
Karambirov <eugenekarambirov@gmail.com>
2018-09-21 09:07:41 +0300

    Create project

---
+++ Podcasts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podcasts
+//
+//  Created by Eugene Karambirov on 21/09/2018.
+//  Copyright © 2018 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
