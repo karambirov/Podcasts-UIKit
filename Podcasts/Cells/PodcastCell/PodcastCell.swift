59917a11e9111767c73fe85207ed6d0e7552e9b7
Karambirov <eugenekarambirov@gmail.com>
2018-09-21 11:13:31 +0300

    Setup header view

--- Podcasts/Cells/PodcastCell/PodcastCell.swift
+++ Podcasts/Cells/PodcastCell/PodcastCell.swift
@@ -7,9 +7,26 @@
 //
 
 import UIKit
+import SDWebImage
 
 class PodcastCell: UITableViewCell {
 
-    
+    // MARK: - Outlets
+    @IBOutlet fileprivate weak var podcastImageView: UIImageView!
+    @IBOutlet fileprivate weak var trackNameLabel: UILabel!
+    @IBOutlet fileprivate weak var artistNameLabel: UILabel!
+    @IBOutlet fileprivate weak var episodeCountLabel: UILabel!
+
+    // maybe force-unwrapping?
+    var podcast: Podcast? {
+        didSet {
+            trackNameLabel.text = podcast?.trackName
+            artistNameLabel.text = podcast?.artistName
+            episodeCountLabel.text = "\(podcast?.trackCount ?? 0) Episodes"
+
+            guard let url = URL(string: podcast?.artworkUrl600 ?? "") else { return }
+            podcastImageView.sd_setImage(with: url)
+        }
+    }
     
 }
--- Podcasts/Controllers/PodcastsSearch/PodcastsSearchController.swift
+++ Podcasts/Controllers/PodcastsSearch/PodcastsSearchController.swift
@@ -30,14 +30,27 @@ final class PodcastsSearchController: UITableViewController {
 
 // MARK: - UITableView
 extension PodcastsSearchController {
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return podcasts.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PodcastCell", for: indexPath) as! PodcastCell
+        cell.podcast = podcasts[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Please, enter a search term."
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        return label
+    }
+
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return podcasts.isEmpty && searchController.searchBar.text?.isEmpty == true ? 250 : 0
     }
 }
 
