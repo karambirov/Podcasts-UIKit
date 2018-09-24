7b830d631e180326dfd07ac6785d45bcba429fa9
Karambirov <eugenekarambirov@gmail.com>
2018-09-24 20:42:23 +0300

    Create type-safe NSNotification names

---
+++ Podcasts/Services/NetworkService.swift
@@ -0,0 +1,10 @@
+//
+//  NetworkService.swift
+//  Podcasts
+//
+//  Created by Eugene Karambirov on 24/09/2018.
+//  Copyright © 2018 Eugene Karambirov. All rights reserved.
+//
+
+import Foundation
+import Alamofire
