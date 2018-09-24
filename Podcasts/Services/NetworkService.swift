2d7551c3fb4b50e0a9d25f8133f6f39ff9ea80b8
Karambirov <eugenekarambirov@gmail.com>
2018-09-24 21:01:55 +0300

    Add fetchPodcasts() request

--- Podcasts/Services/NetworkService.swift
+++ Podcasts/Services/NetworkService.swift
@@ -8,3 +8,42 @@
 
 import Foundation
 import Alamofire
+
+final class NetworkService {
+
+    fileprivate let baseiTunesSearchURL = "https://itunes.apple.com/search"
+
+    static let shared = NetworkService()
+
+}
+
+extension NetworkService {
+
+    func fetchPodcasts(searchText: String, completionHandler: @escaping ([Podcast]) -> Void) {
+        print("\n\t\tSearching for podcasts...")
+
+        let parameters = ["term": searchText, "media": "podcast"]
+
+        Alamofire.request(baseiTunesSearchURL,
+                          method: .get,
+                          parameters: parameters,
+                          encoding: URLEncoding.default,
+                          headers: nil).responseData { dataResponse in
+
+            if let error = dataResponse.error {
+                print("\n\t\tFailed with error:", error)
+                return
+            }
+
+            guard let data = dataResponse.data else { return }
+            do {
+                let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+                completionHandler(searchResult.results)
+            } catch let decodeError {
+                print("\n\t\tFailed to decode:", decodeError)
+            }
+
+        }
+    }
+
+}
