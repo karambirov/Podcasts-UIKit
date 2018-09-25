8e81a23df6429d4670fe659991adb1d6ea65f0de
Karambirov <eugenekarambirov@gmail.com>
2018-09-25 08:29:48 +0300

    Add Episodes model

--- Podcasts/Models/Episode.swift
+++ Podcasts/Models/Episode.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import FeedKit
+
+struct Episode: Codable {
+
+    let title: String
+    let pubDate: Date
+    let description: String
+    let author: String
+    let streamUrl: String
+
+    var fileUrl: String?
+    var imageUrl: String?
+
+    init(feedItem: RSSFeedItem) {
+        self.streamUrl = feedItem.enclosure?.attributes?.url ?? ""
+        self.title = feedItem.title ?? ""
+        self.pubDate = feedItem.pubDate ?? Date()
+        self.description = feedItem.iTunes?.iTunesSubtitle ?? feedItem.description ?? ""
+        self.author = feedItem.iTunes?.iTunesAuthor ?? ""
+        self.imageUrl = feedItem.iTunes?.iTunesImage?.attributes?.href
+    }
+
+}
