a9bc07306eadf478bb94af0e361b24b5f185139e
Karambirov <eugenekarambirov@gmail.com>
2018-09-26 11:34:49 +0300

    Create UI for PlayerDetailsView

---
+++ Podcasts/Views/PlayerDetailsView/PlayerDetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerDetailsView.swift
+//  Podcasts
+//
+//  Created by Eugene Karambirov on 26/09/2018.
+//  Copyright © 2018 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDetailsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
