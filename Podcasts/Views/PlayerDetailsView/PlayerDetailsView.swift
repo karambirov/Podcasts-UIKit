e1978c7ce5ec390f27ce3acca0c6c43a08f830f2
Karambirov <eugenekarambirov@gmail.com>
2018-09-28 13:17:34 +0300

    Add gestures to player details

--- Podcasts/Views/PlayerDetailsView/PlayerDetailsView.swift
+++ Podcasts/Views/PlayerDetailsView/PlayerDetailsView.swift
@@ -18,6 +18,7 @@ import MediaPlayer
 class PlayerDetailsView: UIView {
 
     // MARK: - Properties
+    // MARK: Internal
     var episode: Episode! {
         didSet {
             miniTitleLabel.text = episode.title
@@ -46,7 +47,9 @@ class PlayerDetailsView: UIView {
     }
 
     var playlistEpisodes = [Episode]()
+    var panGesture: UIPanGestureRecognizer!
 
+    // MARK: Fileprivate
     fileprivate let player: AVPlayer = {
         let avPlayer = AVPlayer()
         avPlayer.automaticallyWaitsToMinimizeStalling = false
@@ -102,6 +105,8 @@ class PlayerDetailsView: UIView {
     // MARK: - Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
+
+        setupGestures()
 
         observePlayerCurrentTime()
         observeBoundaryTime()
@@ -257,6 +262,77 @@ extension PlayerDetailsView {
         UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
             self.episodeImageView.transform = self.shrunkenTransform
         })
+    }
+
+}
+
+
+// MARK: - Gestures
+extension PlayerDetailsView {
+
+    // MARK: - Internal
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        if gesture.state == .changed {
+            handlePanChanged(gesture: gesture)
+        } else if gesture.state == .ended {
+            handlePanEnded(gesture: gesture)
+        }
+    }
+
+    @objc func handleMaximize() {
+        UIApplication.mainTabBarController?.maximizePlayerDetails(episode: nil)
+    }
+
+    // MARK: - Fileprivate
+    fileprivate func setupGestures() {
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleMaximize)))
+        panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(gesture:)))
+        miniPlayerView.addGestureRecognizer(panGesture)
+
+        maximizedStackView.addGestureRecognizer(UIPanGestureRecognizer(target: self,
+                                                                       action: #selector(handleDismissalPan(gesture:))))
+    }
+
+    fileprivate func handlePanChanged(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: self.superview)
+        self.transform = CGAffineTransform(translationX: 0, y: translation.y)
+        self.miniPlayerView.alpha = 1 + translation.y / 200
+        self.maximizedStackView.alpha = -translation.y / 200
+    }
+
+    fileprivate func handlePanEnded(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: self.superview)
+        let velocity = gesture.velocity(in: self.superview)
+        print("\n\t\tEnded:", velocity.y)
+
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.transform = .identity
+            if translation.y < -200 || velocity.y < -500 {
+                self.handleMaximize()
+            } else {
+                self.miniPlayerView.alpha = 1
+                self.maximizedStackView.alpha = 0
+            }
+        })
+    }
+
+    @objc fileprivate func handleDismissalPan(gesture: UIPanGestureRecognizer) {
+        print("\n\t\tmiximizedStackView dismissal")
+
+        if gesture.state == .changed {
+            let translation = gesture.translation(in: superview)
+            maximizedStackView.transform = CGAffineTransform(translationX: 0, y: translation.y)
+        } else if gesture.state == .ended {
+            let translation = gesture.translation(in: superview)
+
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.maximizedStackView.transform = .identity
+
+                if translation.y > 50 {
+                    UIApplication.mainTabBarController?.minimizePlayerDetails()
+                }
+            })
+        }
     }
 
 }
