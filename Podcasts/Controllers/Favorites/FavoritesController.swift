34c1628784ff825a61f1ba2211dba09a85785eac
Karambirov <eugenekarambirov@gmail.com>
2018-09-28 15:50:37 +0300

    Add long press gesture to delete

--- Podcasts/Controllers/Favorites/FavoritesController.swift
+++ Podcasts/Controllers/Favorites/FavoritesController.swift
@@ -12,82 +12,58 @@ private let reuseIdentifier = "Cell"
 
 final class FavoritesController: UICollectionViewController {
 
+    // MARK: - Properties
+    fileprivate var podcasts = UserDefaults.standard.savedPodcasts
+
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        setupCollectionView()
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        podcasts = UserDefaults.standard.savedPodcasts
+        collectionView?.reloadData()
+        UIApplication.mainTabBarController?.viewControllers?[1].tabBarItem.badgeValue = nil
     }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
-        return cell
-    }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
 }
 
+// MARK: - UICollectionViewDelegateFlowLayout
 extension FavoritesController: UICollectionViewDelegateFlowLayout {
     
+}
+
+
+// MARK: - Setup
+extension FavoritesController {
+
+    fileprivate func setupCollectionView() {
+        collectionView.backgroundColor = .white
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+        collectionView.addGestureRecognizer(gesture)
+    }
+
+    @objc private func handleLongPress(gesture: UILongPressGestureRecognizer) {
+        let location = gesture.location(in: collectionView)
+        guard let selectedIndexPath = collectionView?.indexPathForItem(at: location) else { return }
+        print(selectedIndexPath.item)
+
+        let alertController = UIAlertController(title: "Remove podcast?", message: nil, preferredStyle: .actionSheet)
+
+        alertController.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { _ in
+            let selectedPodcast = self.podcasts[selectedIndexPath.item]
+            self.podcasts.remove(at: selectedIndexPath.item)
+            self.collectionView?.deleteItems(at: [selectedIndexPath])
+            UserDefaults.standard.deletePodcast(podcast: selectedPodcast)
+        }))
+
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        present(alertController, animated: true)
+    }
+
 }
--- Podcasts/Extensions/UserDefaults+Extensions.swift
+++ Podcasts/Extensions/UserDefaults+Extensions.swift
@@ -13,9 +13,19 @@ extension UserDefaults {
     static let favoritedPodcastKey = "favoritedPodcastKey"
     static let downloadedEpisodesKey = "downloadEpisodesKey"
 
-    func savedPodcasts() -> [Podcast] {
+    var savedPodcasts: [Podcast] {
         guard let savedPodcastsData = UserDefaults.standard.data(forKey: UserDefaults.favoritedPodcastKey) else { return [] }
         guard let savedPodcasts = NSKeyedUnarchiver.unarchiveObject(with: savedPodcastsData) as? [Podcast] else { return [] }
         return savedPodcasts
+    }
+
+    func deletePodcast(podcast: Podcast) {
+        let podcasts = savedPodcasts
+        let filteredPodcasts = podcasts.filter { p -> Bool in
+            return p.trackName != podcast.trackName && p.artistName != podcast.artistName
+        }
+
+        let data = NSKeyedArchiver.archivedData(withRootObject: filteredPodcasts)
+        UserDefaults.standard.set(data, forKey: UserDefaults.favoritedPodcastKey)
     }
 }
