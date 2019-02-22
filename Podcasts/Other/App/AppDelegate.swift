f800584bdae10f1a0b4a3f26625cb153864dc9ec
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-02-22 13:53:58 +0300

    Enable AlamofireNetworkActivityIndicator

--- Podcasts/Other/App/AppDelegate.swift
+++ Podcasts/Other/App/AppDelegate.swift
@@ -7,18 +7,21 @@
 //
 
 import UIKit
+import AlamofireNetworkActivityIndicator
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+                     launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
         window?.makeKeyAndVisible()
         window?.rootViewController = MainTabBarController()
         window?.tintColor = #colorLiteral(red: 0.5176470588, green: 0.2549019608, blue: 0.8666666667, alpha: 1)
+
+        NetworkActivityIndicatorManager.shared.isEnabled = true
 
         return true
     }
