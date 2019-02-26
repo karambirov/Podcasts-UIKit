6d2b66f2bcd33edb456a69f7b1d532c109886dbd
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-02-26 19:39:47 +0300

    Create MainTabBarViewModel

---
+++ Podcasts/Modules/Controllers/MainTabBar/MainTabBarViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarViewModel.swift
+//  Podcasts
+//
+//  Created by Eugene Karambirov on 26/02/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+import Foundation
+
+final class MainTabBarViewModel {
+
+    
+
+}
