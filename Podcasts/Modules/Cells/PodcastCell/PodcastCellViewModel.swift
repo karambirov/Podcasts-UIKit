a136a9a6b01f4b353b9d41498817c4481c3560f3
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-03-12 08:23:48 +0300

    Create PodcastCellViewModel

---
+++ Podcasts/Modules/Cells/PodcastCell/PodcastCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PodcastCellViewModel.swift
+//  Podcasts
+//
+//  Created by Eugene Karambirov on 12/03/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+import Foundation
+
+final class PodcastCellViewModel {
+
+    
+
+}
