e66da34b054428072068d6bdc31ff09a5e479d3f
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-03-13 16:58:21 +0300

    Setup subviews

--- Podcasts/Modules/Views/PodcastsSearchingView/PodcastsSearchingView.swift
+++ Podcasts/Modules/Views/PodcastsSearchingView/PodcastsSearchingView.swift
@@ -8,4 +8,42 @@
 
 import UIKit
 
-final class PodcastsSearchingView: UIView {}
+final class PodcastsSearchingView: UIView {
+
+    // MARK: - Properties
+    fileprivate lazy var activityIndicatorView = UIActivityIndicatorView(style: .whiteLarge)
+    fileprivate lazy var textLabel      = UILabel()
+    fileprivate lazy var stackView   = UIStackView(arrangedSubviews: [activityIndicatorView, textLabel])
+
+    // MARK: - Life cycle
+    override func didMoveToSuperview() {
+        setupViews()
+    }
+
+}
+
+// MARK: - Setup views
+extension PodcastsSearchingView {
+
+    fileprivate func setupViews() {
+        setupLabel()
+        setupLayout()
+    }
+
+    private func setupActivityIndicatorView() {
+        activityIndicatorView.startAnimating()
+        activityIndicatorView.color = .darkGray
+    }
+
+    private func setupLabel() {
+        textLabel.numberOfLines = 0
+        textLabel.textColor = R.color.tintColor()
+        textLabel.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        textLabel.text = "Currently searching, please wait"
+    }
+
+    private func setupLayout() {
+        
+    }
+
+}
