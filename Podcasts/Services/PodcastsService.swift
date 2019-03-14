503d93d425b3f14ea186d1e9e4e92f357d9c545a
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-03-14 11:18:14 +0300

    Create PodcastsService.swift

---
+++ Podcasts/Services/PodcastsService.swift
@@ -0,0 +1,15 @@
+//
+//  PodcastsService.swift
+//  Podcasts
+//
+//  Created by Eugene Karambirov on 14/03/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+import Foundation
+
+final class PodcastsService {
+
+    
+
+}
