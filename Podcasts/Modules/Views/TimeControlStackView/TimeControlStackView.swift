adbf9e257998c25c54cec2fb144daa14964bbaeb
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-03-18 21:51:33 +0300

    Rename TimeControlView to TimeControlStackView

---
+++ Podcasts/Modules/Views/TimeControlStackView/TimeControlStackView.swift
@@ -0,0 +1,19 @@
+//
+//  TimeControlView.swift
+//  Podcasts
+//
+//  Created by Eugene Karambirov on 18/03/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+
+final class TimeControlStackView: UIStackView {
+
+    // MARK: - Properties
+    fileprivate lazy var currentTimeSlider = UISlider()
+    fileprivate lazy var currentTimeLabel = UILabel()
+    fileprivate lazy var durationLabel = UILabel()
+    fileprivate
+
+}
