0aa361d64cdf8d5994d249a1273d3686aefcab02
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-03-18 21:49:37 +0300

    Create PlayingControlView.swift

---
+++ Podcasts/Modules/Views/PlayingControlView/PlayingControlView.swift
@@ -0,0 +1,15 @@
+//
+//  PlayingControlView.swift
+//  Podcasts
+//
+//  Created by Eugene Karambirov on 18/03/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+
+final class PlayingControlView: UIView {
+
+
+
+}
