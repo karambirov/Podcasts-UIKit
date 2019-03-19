28310ece061f6f59cf7c432bb8a5b894a5fe9cf9
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-03-19 16:52:13 +0300

    Create VolumeControlStackView.swift

---
+++ Podcasts/Modules/Views/VolumeControlStackView/VolumeControlStackView.swift
@@ -0,0 +1,16 @@
+//
+//  VolumeControlStackView.swift
+//  Podcasts
+//
+//  Created by Eugene Karambirov on 19/03/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+final class VolumeControlStackView: UIStackView {
+
+
+
+}
