0e7cccf196cbbc794c63a935a5f44a12bd8c2abd
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-04-07 17:34:34 +0300

    Create PlayerDetails VC and VM

---
+++ Podcasts/Modules/View Controllers/PlayerDetails/PlayerDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlayerDetailsViewController.swift
+//  Podcasts
+//
+//  Created by Eugene Karambirov on 07/04/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
